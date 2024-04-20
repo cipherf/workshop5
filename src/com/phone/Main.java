5b9209156bac8ab58d2fe13dd458bdc23cc07dc6
parsaex7 <parsaexir2005@gmail.com>
2024-04-20 17:58:11 +0330

    add my information

--- src/com/phone/Main.java
+++ src/com/phone/Main.java
@@ -1,7 +1,7 @@
 package com.phone;
 
 import java.util.Scanner;
-
+//Parsa Exir - 40231403
 public class Main {
     public static void main(String[] args) {
         Phonebook phonebook = new Phonebook();
